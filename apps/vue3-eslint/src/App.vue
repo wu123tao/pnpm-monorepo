158ca89d17b9512625dc5a33dce0c1bcda5ee1d7
wutao <q.1041154165@qq.com>
2023-07-18 17:29:05 +0800

    chore: 初始化仓库

---
+++ apps/vue3-eslint/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <a target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { sum } from '@pnpm-monorepo/utils';
+console.log(sum(1, 2));
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+}
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ packages/utils/src/sum.ts
@@ -0,0 +1,10 @@
+/**
+ * 求和
+ * @param a 整数
+ * @param b 整数
+ */
+export function sum(a: number, b: number) {
+    console.log(123);
+
+    return a + b;
+}
