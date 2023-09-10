75922d8a5a8e801dad46d39952d6da6312e9c22e
wutao <q.1041154165@qq.com>
2023-09-10 19:55:08 +0800

    fix: 修改分片上传

--- apps/vue3-eslint/src/App.vue
+++ apps/vue3-eslint/src/App.vue
@@ -5,116 +5,53 @@
             <el-button type="primary">Click to upload</el-button>
         </el-upload>
     </div>
-
-    <div>
-        <el-alert title="分片上传-（文件最终合并到本地服务上）" type="success" :closable="false" />
-        <el-upload :show-file-list="false" :http-request="uploadLocalServer">
-            <el-button type="primary">Click to upload</el-button>
-
-            <template v-if="fileData.name" #tip>
-                <div>{{ fileData.name }}</div>
-                <el-progress :percentage="percentage" />
-            </template>
-        </el-upload>
-    </div>
 </template>
 
 <script setup lang="ts">
-import { post } from '@/api/index';
-import type { HttpResponse } from '@pnpm-monorepo/utils';
-import { v4 as uuid } from 'uuid';
+import type { UploadRequestOptions } from 'element-plus';
 
-interface FileData {
-    name: string;
-    size: number;
-    uuid: string;
-    suffix: string;
-}
+const chunkSize = ref<number>(1024 * 50);
+const threadCount = ref<number>(navigator.hardwareConcurrency | 4);
 
-const chunkSize = 1 * 1024 * 1024; // 切片大小Mb
-const percentage = ref<number>(0);
-const fileData = ref<FileData>({
-    name: '',
-    size: 0,
-    uuid: '',
-    suffix: '',
-});
+async function handleChange(uploadRequestOptions: UploadRequestOptions) {
+    const { file } = uploadRequestOptions;
 
-watch(
-    () => percentage.value,
-    (value) => {
-        console.log(value);
-    }
-);
-
-/**
- * 文件上传到本地服务
- */
-async function uploadLocalServer(uploadFile: { file: File }) {
-    const nameList = uploadFile.file.name.split('.');
-
-    fileData.value.name = uploadFile.file.name;
-    fileData.value.size = uploadFile.file.size;
-    fileData.value.uuid = uuid();
-    fileData.value.suffix = nameList[nameList.length - 1];
-
-    percentage.value = 0;
-    const chunkCount = Math.ceil(fileData.value.size / chunkSize);
-    console.log(chunkCount, '切片数量');
-
-    for (let i = 0; i < chunkCount; i++) {
-        const res = await uploadChunkFileLocalServer(i, uploadFile.file);
-        console.log(res);
-    }
-
-    const res = await post('files/mergeFile', {
-        fileName: `${fileData.value.uuid}-${fileData.value.name}`,
-        suffix: fileData.value.suffix,
-        uuid: fileData.value.uuid,
-    });
+    const res = await cutFile(file);
     console.log(res);
 }
 
-async function uploadChunkFileLocalServer(
-    i: number,
-    file: File
-): Promise<HttpResponse<{ data: string }>> {
-    const start = i * chunkSize;
-    const end = Math.min(fileData.value.size as number, start + chunkSize);
+async function cutFile(file: File) {
+    return new Promise((resolve) => {
+        const chunks: any = [];
 
-    const chunkFile = file.slice(start, end);
+        const chunkCount = Math.ceil(file.size / chunkSize.value);
 
-    const formData = new FormData();
-    formData.append('fileName', `${fileData.value.name}@@${i}`);
-    formData.append('file', chunkFile);
-    formData.append('uuid', fileData.value.uuid);
+        const workChunkCount = Math.ceil(chunkCount / threadCount.value);
 
-    return await post('files/chunkUpload', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-        onUploadProgress: (evt) => {
-            percentage.value = Number(
-                (
-                    (Math.min(fileData.value.size, start + evt.loaded) / fileData.value.size) *
-                    100
-                ).toFixed(2)
-            );
-        },
+        let finishCount = 0;
+
+        for (let i = 0; i < threadCount.value; i++) {
+            const chunkWorker = new Worker(new URL('./worker.ts', import.meta.url), {
+                type: 'module',
+            });
+            const startIndex = i * workChunkCount;
+            let endIndex = startIndex + workChunkCount;
+            if (endIndex > chunkCount) {
+                endIndex = chunkCount;
+            }
+            chunkWorker.postMessage({ file, chunkSize: chunkSize.value, startIndex, endIndex });
+
+            chunkWorker.onmessage = (e) => {
+                for (let i = startIndex; i < endIndex; i++) {
+                    chunks[i] = e.data[i - startIndex];
+                }
+                chunkWorker.terminate();
+                finishCount++;
+                if (finishCount === threadCount.value) {
+                    resolve(chunks);
+                }
+            };
+        }
     });
-}
-
-/**
- * 单文件上传
- */
-async function handleChange(uploadFile: { file: File }) {
-    const formData = new FormData();
-    formData.append('file', uploadFile.file);
-
-    const res = await post('files/upload', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-        onUploadProgress: (evt) => {
-            console.log(evt.loaded);
-        },
-    });
-    console.log(res);
 }
 </script>
---
+++ apps/vue3-eslint/src/worker.ts
@@ -0,0 +1,31 @@
+import SparkMD5 from 'spark-md5';
+
+self.onmessage = async (e) => {
+    const proms = [];
+
+    const { file, chunkSize, startIndex, endIndex } = e.data;
+
+    console.log(file, chunkSize, startIndex, endIndex);
+
+    for (let i = startIndex; i < endIndex; i++) {
+        proms.push(createChunk(file, i, chunkSize));
+    }
+    const chunks = await Promise.all(proms);
+    self.postMessage(chunks);
+};
+
+function createChunk(file: File, index: number, chunkSize: number) {
+    return new Promise((resolve) => {
+        const start = index * chunkSize;
+        const end = start + chunkSize;
+
+        const sparkMD5 = new SparkMD5();
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            sparkMD5.append(e.target?.result as string);
+            resolve({ start, end, index, hash: sparkMD5.end() });
+        };
+        reader.readAsArrayBuffer(file.slice(start, end));
+    });
+}
